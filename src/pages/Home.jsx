699a7d9096dc6996aeba3033d5c81bece161773e
Francisco Zuniga <fzuniga325@gmail.com>
2023-07-13 21:32:00 -0500

    more changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,11 +7,11 @@ const Home = () => {
   return (
     <>
         <Main />
-        <Row rowID='1' title='UpComing' fetchURL={requests.requestUpcoming} />
-        <Row rowID='2' title='Popular' fetchURL={requests.requestPopular} />
+        <Row rowID='1' title='Popular' fetchURL={requests.requestPopular} />
+        <Row rowID='2' title='Top Rated' fetchURL={requests.requestTopRated} />
         <Row rowID='3' title='Trending' fetchURL={requests.requestTrending} />
-        <Row rowID='4' title='Top Rated' fetchURL={requests.requestTopRated} />
-        <Row rowID='5' title='Anime' fetchURL={requests.requestAnime} />
+        <Row rowID='4' title='Anime' fetchURL={requests.requestAnime} />
+        <Row rowID='5' title='Upcoming' fetchURL={requests.requestUpcoming} />
     </>
   )
 }
